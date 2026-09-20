7ce7bb2c9c0e39b140234f87226d93aa03d5f174
784988787aaaa-eng <784988787aaaa@gmail.com>
2026-09-21 02:42:52 +0300

    fix: use available report header icon

--- app/src/main/java/com/smartledger/aldaftar/ui/screens/habayeb/components/ComprehensiveReportDialog.kt
+++ app/src/main/java/com/smartledger/aldaftar/ui/screens/habayeb/components/ComprehensiveReportDialog.kt
@@ -140,7 +140,7 @@ fun ComprehensiveReportDialog(
             ) {
                 MizanDialogHeader(
                     title = stringResource(id = R.string.drawer_comprehensive_report_label),
-                    icon = Icons.Default.Description,
+                    icon = Icons.Default.Share,
                     iconTint = activeThemeColor,
                     onCloseClick = dismissDialog
                 )
